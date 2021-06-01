baee629ff5ec01406f42d785a4d0f831a0a50173
bilalraza786 <80530063+bilalraza786@users.noreply.github.com>
2021-06-01 20:01:10 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and completed successfully.
+          Edit <code>src/App.js</code> and completed by Bilal Raza successfully.
         </p>
         <a
           className="App-link"
